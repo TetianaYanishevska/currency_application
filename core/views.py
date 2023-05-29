2060ea9e3155cd08f59f07e0559e90c04a601011
TetianaYanishevska <yana7nikol@gmail.com>
2023-05-30 00:53:38 +1200

    add recording to database

--- core/views.py
+++ core/views.py
@@ -1,10 +1,22 @@
+import datetime
+
 from django.shortcuts import render
-from currency.services import ExchangeRateService
+from currency.services import ProviderService, ExchangeRateService
 
 
 # Create your views here.
 def index(request):
-    service = ExchangeRateService()
-    rates = service.get_rates()
-    print(rates)
+    providers_data = [
+        {'name': 'Privatbank', 'api_url': 'https://api.privatbank.ua/p24api/exchange_rates'}
+    ]
+    start_date = datetime.datetime(2023, 1, 1)
+    end_date = datetime.datetime.now()
+
+    for data in providers_data:
+        provider_service = ProviderService(name=data['name'], api_url=data['api_url'])
+        service = ExchangeRateService(provider=provider_service.create_provider(),
+                                      start_date=start_date,
+                                      end_date=end_date)
+        rates = service.get_rates()
+        print(rates)
     return render(request, 'core/index.html')
--- currency/services.py
+++ currency/services.py
@@ -1,17 +1,43 @@
 import requests
+from datetime import datetime, timedelta
+
+
+from currency.models import ExchangeRateProvider, ExchangeRate
+
+
+class ProviderService:
+    def __init__(self, name, api_url):
+        self.name = name
+        self.api_url = api_url
+
+    def create_provider(self):
+
+        if ExchangeRateProvider.objects.filter(name=self.name, api_url=self.api_url).exists():
+            print(f"Provider {self.name} is already exists")
+            provider = ExchangeRateProvider.objects.get(name=self.name, api_url=self.api_url)
+        else:
+            provider = ExchangeRateProvider(name=self.name, api_url=self.api_url)
+            provider.save()
+            print(f"Provider {self.name} was successfully created")
+        return provider
 
 
 class ExchangeRateService:
+
     CURRENCIES = ['GBP', 'USD', 'CHF', 'EUR']
 
-    def get_rates(self):
-        url = "https://api.privatbank.ua/p24api/exchange_rates"
+    def __init__(self, provider, start_date, end_date):
+        self.provider = provider
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def get_rate(self, date):
 
         params = {
-            'date': '14.05.2023'
+            'date': date.strftime('%d.%m.%Y')
         }
 
-        response = requests.get(url, params=params)
+        response = requests.get(self.provider.api_url, params=params)
         data = response.json()
         rates = data['exchangeRate']
         currency_rates = []
@@ -20,14 +46,29 @@ class ExchangeRateService:
         for r in rates:
             if r['currency'] not in self.CURRENCIES:
                 continue
-            currency_rates.append(
-                {
+            currency_rate = {
                     'base_currency': base_currency,
                     'currency': r['currency'],
                     'date': date,
                     'sale_rate': r['saleRate'],
                     'buy_rate': r['purchaseRate']
                 }
-            )
+
+            currency_rates.append(currency_rate)
+            exchange_rate = ExchangeRate(date=datetime.strptime(date, '%d.%m.%Y').strftime('%Y-%m-%d'),
+                                         base_currency=base_currency, currency=r['currency'],
+                                         sale_rate=r['saleRate'], buy_rate=r['purchaseRate'], provider=self.provider
+                                         )
+            exchange_rate.save()
+
         return currency_rates
 
+    def get_rates(self):
+
+        current_date = self.start_date
+        delta = timedelta(days=1)
+
+        while current_date <= self.end_date:
+            currency_rates = self.get_rate(date=current_date)
+            print(currency_rates)
+            current_date += delta
